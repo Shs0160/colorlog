5e7a84d373e5429aa6af263213add9b70b77d830
Shs0160 <aladinsue@gmail.com>
2024-05-21 02:31:30 +0900

    react API update

--- src/main/java/color_log/colorlog/WebConfig.java
+++ src/main/java/color_log/colorlog/WebConfig.java
@@ -2,6 +2,7 @@ package color_log.colorlog;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -15,5 +16,10 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("*"); // 모든 헤더 허용
 
     }
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/user/{userId}").setViewName("forward:/index.html");
+    }
+
 
 }
--- src/main/java/color_log/colorlog/controller/HomeController.java
+++ src/main/java/color_log/colorlog/controller/HomeController.java
@@ -2,6 +2,7 @@ package color_log.colorlog.controller;
 
 import color_log.colorlog.domain.PhotoGroup;
 import color_log.colorlog.service.UserService;
+import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,7 +13,7 @@ import org.springframework.ui.Model;
 import color_log.colorlog.domain.User;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.core.JsonProcessingException;
-
+import org.springframework.web.servlet.view.RedirectView;
 
 
 import java.io.IOException;
@@ -45,9 +46,14 @@ public class HomeController {
     }*/
 
     @GetMapping("/user/{userId}")
-    public ResponseEntity<User> userPage(@PathVariable Long userId){
-        User user = userService.getUserById(userId);
-        return ResponseEntity.ok(user);
+    public RedirectView userPage(@PathVariable Long userId, HttpServletResponse response) {
+
+        String reactDevServer = "http://192.168.0.75:3000"; // React 개발 서버 URL
+        return new RedirectView(reactDevServer + "/user/" + userId);
+        /*User user = userService.getUserById(userId);
+        return ResponseEntity.ok(user);*/
+
+    }
        /* ObjectMapper objectMapper = new ObjectMapper();
         try {
             String userJson = objectMapper.writeValueAsString(user);
@@ -56,7 +62,7 @@ public class HomeController {
             e.printStackTrace();
             return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing JSON");
         }*/
-    }
+
 
 
 }
