4f4ee6d55e85fb352d2b7ce1d3a074c7f24db034
Shs0160 <aladinsue@gmail.com>
2024-09-09 01:02:09 +0900

    tir clone

---
+++ src/main/java/color_log/colorlog/SwaggerConfig.java
@@ -0,0 +1,22 @@
+package color_log.colorlog;
+
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import lombok.RequiredArgsConstructor;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@OpenAPIDefinition(
+        info = @Info(title = "Colorlog",
+                description = "colorlog api명세서",
+                version = "v1"))
+@RequiredArgsConstructor
+@Configuration
+public class SwaggerConfig {
+
+
+}
