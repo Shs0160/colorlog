30e4a02e44b0d1da0f424d6857be5f3e82184bfd
Shs0160 <aladinsue@gmail.com>
2024-09-09 21:49:07 +0900

    Swagger Config OpenAPI add

--- src/main/java/color_log/colorlog/SwaggerConfig.java
+++ src/main/java/color_log/colorlog/SwaggerConfig.java
@@ -5,6 +5,8 @@ import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
 import lombok.RequiredArgsConstructor;
 import org.springdoc.core.models.GroupedOpenApi;
 import org.springframework.context.annotation.Bean;
@@ -17,6 +19,29 @@ import org.springframework.context.annotation.Configuration;
 @RequiredArgsConstructor
 @Configuration
 public class SwaggerConfig {
+
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("colorlog-public")
+                .pathsToMatch("/**")  // Swagger에 표시될 경로를 지정합니다.
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+                .components(new Components())
+                .info(new io.swagger.v3.oas.models.info.Info()
+                        .title("Colorlog API")
+                        .version("v1")
+                        .description("colorlog api명세서")
+                        .contact(new Contact()
+                                .name("songheesu")
+                                .url("http://localhost:8080")
+                                .email("bsthss0160@gmail.com"))
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org")));
+    }
 
 
 }
