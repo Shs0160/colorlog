7b6d7f4bc4e42bcaf9697311d25ce0ea12b147e6
Shs0160 <aladinsue@gmail.com>
2024-09-21 00:21:03 +0900

    webconfig localhost add

--- src/main/java/color_log/colorlog/WebConfig.java
+++ src/main/java/color_log/colorlog/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/api/**")
-                .allowedOrigins("http://colorlog.site", "https://colorlog.site")
+                .allowedOrigins("http://colorlog.site", "https://colorlog.site", "http://localhost:3000")
                 .allowedMethods("GET", "POST", "PUT", "DELETE")
                 .allowedHeaders("*");
 
